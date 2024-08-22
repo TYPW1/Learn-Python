24120e4e3fe9577255ad7b07913619237d2976c3
TYPW01 <pierretypw@gmail.com>
2024-08-22 14:23:48 +0200

    tried out some ast visitors

--- function_scope.py
+++ function_scope.py
@@ -1,12 +1,11 @@
 import ast
-
-code = """
-class pete:
-    def greet(name):
-        print(f'Hello, {name}!')
-"""
+import json
+from os.path import join
+from pathlib import Path
 
 
+
+'''
 class FunctionVisitor(ast.NodeVisitor):
     def __init__(self):
         self.functions = []
@@ -15,7 +14,7 @@ class FunctionVisitor(ast.NodeVisitor):
         # print(f"Function name: {node.name}")
         # print(ast.dump(node, indent=4, include_attributes=True))
         self.functions.append(nodes)
-        # self.generic_visit(node)
+        self.generic_visit(nodes)
 
 
 def parse(codes):
@@ -34,3 +33,68 @@ def extract_node(tree):
 functions = []
 for node in extract_node(parse(code)):
     print(ast.dump(node, indent=4, include_attributes=True))
+'''
+
+DEFAULT_OUTPUT_FILE = join(Path(__file__).parent, 'results.json')
+
+
+class ExpressionsVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.expressions = []
+
+    def visit_FunctionDef(self, node):
+        self.expressions.append(node)
+        self.generic_visit(node)
+
+
+def parse(codes):
+    return ast.parse(codes)
+
+
+def extract_expressions(tree):
+    visitor = ExpressionsVisitor()
+    visitor.visit(tree)
+    return visitor.expressions
+
+# Function to convert AST node to a dictionary
+def node_to_dict(node):
+    # If the node is an AST object, convert it to a dictionary
+    if isinstance(node, ast.AST):
+        fields = {key: node_to_dict(value) for key, value in ast.iter_fields(node)}
+        attributes = {key: getattr(node, key) for key in node._attributes if hasattr(node, key)}
+        return {
+            'type': node.__class__.__name__,
+            **fields,
+            **attributes
+        }
+    # If it's a list, convert each item
+    elif isinstance(node, list):
+        return [node_to_dict(item) for item in node]
+    # Otherwise, return the node's value
+    else:
+        return node
+
+code = """
+def divide(a, b):
+    try:
+        result = a / b
+        result = a / b
+        result = a / b
+    except ZeroDivisionError:
+        return "Cannot divide by zero"
+    return result
+
+print("Division result:", divide(10, 2))
+print("Division result:", divide(10, 0))
+
+"""
+
+nodes = extract_expressions(parse(code))
+
+dumped_nodes = [node_to_dict(node) for node in nodes]
+
+for node in extract_expressions(parse(code)):
+    print(ast.dump(node, indent=4, include_attributes=True))
+
+with open(DEFAULT_OUTPUT_FILE, 'w') as outfile:
+    json.dump(dumped_nodes, outfile, indent=4)
