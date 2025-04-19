79e3722b3512177083eeee60271059572951bd34
Pierr <pierretypw@gmail.com>
2025-04-19 11:43:02 +0200

    feat: initialize project with configuration files and dependencies

--- Intermediate/us_state_quiz/test.py
+++ Intermediate/us_state_quiz/test.py
@@ -1,3 +1,11 @@
 name = "peter willy"
 p = name.title()
-print(p)
+#print(p)
+import random
+names = ["peter", "willy", "john", "doe", "jane", "smith", "alice", "bob"]
+student_scores={student:random.randint(1,100) for student in names}
+
+passed_students = {student:score for student,score in student_scores.items() if score > 50}
+
+for name in passed_students.keys():
+    print(name)
