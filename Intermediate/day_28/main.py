b925817945f49cee59cb54fa4bbdb63c547fbef4
Pierr <pierretypw@gmail.com>
2025-05-01 15:44:31 +0200

    Revert "feat: add user entry form and implement countdown timer functionality"
    This reverts commit b60a7c5d61a7631c3c027de675b0566313194edc.

--- Intermediate/day_28/main.py
+++ Intermediate/day_28/main.py
@@ -1,5 +1,4 @@
 import time
-import math
 from tkinter import *
 
 from sympy.abc import lamda
@@ -20,44 +19,31 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-def start_timer():
-    count_down(3)
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def count_down(count):
-
-    count_min = math.floor(count / 60)
-    count_sec = count % 60
-
-    if count_sec == 0:
-        count_sec = "00"
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    if count > 0:
-        window.after(1000, count_down, count - 1)
 
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-
+window.after(1000,)
 
 canvas = Canvas(width=200, height=224, highlightthickness=0, bg=YELLOW)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-
-timer_label = Label()
-timer_label.config(text="Timer", fg= GREEN, bg=YELLOW, font=(FONT_NAME, 24, "bold") )
-timer_label.grid(column=1, row=0)
+timer_text = Label()
+timer_text.config(text="Timer", fg= GREEN, bg=YELLOW, font=(FONT_NAME, 24, "bold") )
+timer_text.grid(column=1, row=0)
 
 start_button = Button()
-start_button.config(text="Start", command= start_timer)
+start_button.config(text="Start", command= lambda: print("start"))
 start_button.grid(column=0, row=2)
 
 reset_button = Button()
