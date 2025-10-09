703e3ffc2f57beb613d4e0807da645b63fafcdfe
typw1 <pierretypw@gmail.com>
2025-10-09 16:01:39 +0200

    feat: implement password generation and add phone number extraction functionality

--- Intermediate/day 29/main.py
+++ Intermediate/day 29/main.py
@@ -1,15 +1,37 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+password_list = [random.choice(letters) for _ in range(nr_letters)] + \
+                [random.choice(symbols) for _ in range(nr_symbols)] + \
+                [random.choice(numbers) for _ in range(nr_numbers)]
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    # Check if website is valid
+    # Check if wy<------§ebsite is valid
     if not website or not website_entry.get().isalnum():
         messagebox.showerror(title="Invalid Website", message="Please enter a valid website name.")
         return
--- Scripts/Standalone/isphoneNumber.py
+++ Scripts/Standalone/isphoneNumber.py
@@ -16,5 +16,9 @@ def isPhoneNumber(text):
             return False
     return True
 
-print(isPhoneNumber('415-555-4242'))
-print(isPhoneNumber('Moshi moshi'))
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        number = 'Phone number found: ' + chunk
+
