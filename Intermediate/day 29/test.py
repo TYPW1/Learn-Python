1ac459cef39b54563ac8c8787e1313761d1779e2
typw1 <pierretypw@gmail.com>
2025-10-20 10:46:01 +0200

    still working on credentials submition form

--- Intermediate/day 29/test.py
+++ Intermediate/day 29/test.py
@@ -1,1 +1,10 @@
-print ("hello world")
+for i in range (3, 10):
+    pass
+
+num = [i for i in range(3, 10)]  # generator expression
+print(num)  # <generator object <genexpr> at 0x000001E
+
+num = []
+for i in range (3, 10):
+    num.append(i)   
+print(num)  # [3, 4, 5, 6, 7, 8, 9]
