b428c4a89db51d6114e63b5e85c800313acecb89
typw1 <pierretypw@gmail.com>
2025-10-27 15:54:50 +0100

    Refactor test.py: Comment out BMI calculation and add like counting functionality

--- Intermediate/day 30/test.py
+++ Intermediate/day 30/test.py
@@ -17,10 +17,50 @@
 #     except B:
 #         print("B")
 
-height = float(input("Height: "))
-weight = int(input("Weight: "))
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
 
-if height > 3:
-    raise ValueError("Human height should not be over 3 meters.")
-bmi = weight / height ** 2
-print(bmi)
+# if height > 3:
+#     raise ValueError("Human height should not be over 3 meters.")
+# bmi = weight / height ** 2
+# print(bmi)
+
+# my_dict = [{"name": "Pierre", "age": 30}, 
+#            {"name": "Marie", "age": 25}, 
+#            { "age": 27},
+#            {"name": "Jean", "age": 40},
+#            {"name": "Luc", "age": 22},
+#            {"age": 35}]
+
+# def print_name(list_of_names):
+#     for name in list_of_names:
+#         print(name["name"])
+
+# try:
+#     print_name(my_dict)
+# except Exception as e:
+#     print(f"no name in this dict")
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+
+def count_likes(posts):
+
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except:
+            total_likes+=0
+    
+    return total_likes
+
+
+print(count_likes(facebook_posts))
