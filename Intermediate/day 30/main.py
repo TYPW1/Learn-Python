7966f4fecf6ef1bf791292254abf6569ed097549
typw1 <pierretypw@gmail.com>
2025-10-27 16:14:55 +0100

    Add NATO phonetic alphabet functionality and data file
    - Implemented a phonetic alphabet dictionary using data from a CSV file.
    - Added user input functionality to convert letters to phonetic code words.
    - Created a new CSV file containing the phonetic alphabet mapping.

--- Intermediate/day 30/main.py
+++ Intermediate/day 30/main.py
@@ -1,20 +1,20 @@
 # with open ("b_file.txt", "r") as file:
 #     content = file.read()
 
-try:
-    file = open("b_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("b_file.txt", "w")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+# try:
+#     file = open("b_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("b_file.txt", "w")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed.")
 
 # while True:
 #     try:
@@ -22,3 +22,11 @@ finally:
 #         break
 #     except ValueError:
 #         print("Oops!  That was no valid number.  Try again...")
+
+import pandas
+data =pandas.read_csv("nato_phonetic_alphabet.csv")
+phoenetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+print(phoenetic_dict)
+word = input("Enter a word: ").upper()
+output_list = [phoenetic_dict[letter] for letter in word]
+print(output_list)
